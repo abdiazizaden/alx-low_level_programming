22075ce020740672eb9bf5d8814bbc52bcda55d7
Adamsfour <fourabdi4@gmail.com>
2023-02-17 21:04:57 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  *  main - A c program that prints a line to the standard error
- *  description is this
+ *  A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *  Return 1 (Success)
  */
 int main(void)
