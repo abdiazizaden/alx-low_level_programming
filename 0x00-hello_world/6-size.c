f443cbfd6127ac8e7b7ab4287398eca949c395fe
abdiazizaden <fourabdi4@gmail.com>
2023-02-17 09:32:56 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints the sixe of various types 
- * on the computer it is compiled and run on..
- * Return: Always 0.
+ * main - A program that prints the sixe of various types
+ * on the computer it is compiled and run on
+ * Return: Always 0
  */
 int main(void)
 {
